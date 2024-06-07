7f21a49e5222524648f2b57e0575856f19538c62
Traian Sandu <tsandu@deloittece.com>
2024-06-07 10:27:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ def count(text):
     count = len(words_list)
     return count
 
+def letter_counter(text):
+    map = {}
+    lowered_string = text.lower()
+    for i in lowered_string:
+        if i in map:
+            map[i] += 1
+        else:
+            map[i] = 1
+    return map
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         word_count = count(file_contents)
-    print(f"{word_count} is the number of words") 
+    print(f"{word_count} is the number of words")
+    print(letter_counter(file_contents)) 
 
 
 main()
